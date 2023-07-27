ba53123425ad37ab6577e4b05ad7e01a4208c015
elcodo254 <ellawachaga@gmail.com>
2023-07-27 01:37:43 +0000

    debug:missing `(` and function

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
-export default getFullResponseFromAPI(success) {
-  return new Promise(resolve, reject) => {
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
