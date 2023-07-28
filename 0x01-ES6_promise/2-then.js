e55379d63ab41d1c33c40d661ad18c7d0d991d4e
elcodo254 <ellawachaga@gmail.com>
2023-07-28 23:49:55 +0000

    fix lint bug

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ export default function handleResponseFromAPI(promise) {
 
   return promise
     .then(() => body)
-    .catch(() => new Erro())
+    .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
