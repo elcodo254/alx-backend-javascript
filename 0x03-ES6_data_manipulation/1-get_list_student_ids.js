2d96cbec9d6b2837f762b6f92f922a95efa335f4
elcodo254 <ellawachaga@gmail.com>
2023-09-12 15:19:45 +0000

    task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const ids = students.map((item) => item.id);
+  return ids;
+}
+
+export default getListStudentIds;
