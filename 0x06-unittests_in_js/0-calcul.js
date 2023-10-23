c8df7ad48ffe35c40411865e3defd7d3b4653910
elcod254 <ellawachaga@gmail.com>
2023-10-23 16:03:07 -0700

    update test file

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -3,3 +3,21 @@ function calculateNumber(a, b) {
 };
 
 module.exports = calculateNumber;
+module.exports = function calculateNumber(a, b = 0) {
+  const aNum = Number(a);
+  const bNum = Number(b);
+  
+  if (Number.isNaN(aNum) || Number.isNaN(bNum))
+    throw TypeError('Parameters must be numbers');
+  
+    return Math.round(aNum) + Math.round(bNum);
+  };:x
+module.exports = function calculateNumber(a, b = 0) {
+  const aNum = Number(a);
+  const bNum = Number(b);
+  
+  if (Number.isNaN(aNum) || Number.isNaN(bNum))
+    throw TypeError('Parameters must be numbers');
+  
+    return Math.round(aNum) + Math.round(bNum);
+  };
