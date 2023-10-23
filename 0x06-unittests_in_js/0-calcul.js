a43b8f473d26885b1ab28afa7c5960ec24b05ed5
elcod254 <ellawachaga@gmail.com>
2023-10-23 16:07:36 -0700

    fix redundancy in 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,23 +1,9 @@
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
+module.exports = function calculateNumber(a, b = 0) {
+  const aNum = Number(a);
+  const bNum = Number(b);
+  
+  if (Number.isNaN(aNum) || Number.isNaN(bNum))
+    throw TypeError('Parameters must be numbers');
+  
+    return Math.round(aNum) + Math.round(bNum);
 };
-
-module.exports = calculateNumber;
-module.exports = function calculateNumber(a, b = 0) {
-  const aNum = Number(a);
-  const bNum = Number(b);
-  
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError('Parameters must be numbers');
-  
-    return Math.round(aNum) + Math.round(bNum);
-  };:x
-module.exports = function calculateNumber(a, b = 0) {
-  const aNum = Number(a);
-  const bNum = Number(b);
-  
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError('Parameters must be numbers');
-  
-    return Math.round(aNum) + Math.round(bNum);
-  };
